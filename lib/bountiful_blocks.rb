46a9526299c057235a93a54db6db6b9e1f7a7da5
Riccardo Agatea <riccardo@moku.io>
2023-09-07 11:10:02 +0200

    remove error class

--- lib/bountiful_blocks.rb
+++ lib/bountiful_blocks.rb
@@ -1,8 +1,4 @@
 require_relative 'bountiful_blocks/version'
 
-module BountifulBlocks
-  class Error < StandardError; end
-end
-
 require_relative 'bountiful_blocks/multiblock'
 require_relative 'bountiful_blocks/kernel_patch'
